0eb1ee7ab50229552d5bf61bac8c3ef49e866306
翁成 <devwong@qq.com>
2023-09-21 17:24:09 +0800

    first commit3

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 设置 npm registry
-npm config set registry https://registry.npm.taobao.org
 
-npm run serve
+npm config set registry https://registry.npm.taobao.org \
+&& npm install \
+&& npm run serve 
